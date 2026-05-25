bcec5537c616759a4c223a041df42c36f1d08c1b
GlerschNersch <mvincent43@gmail.com>
2026-05-24 17:39:09 -0700

    chore: remove debug files

--- cabinet_bridge/fix_homearcade.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('client/src/components/dashboard-themes/HomeArcadeTheme.tsx', 'utf8');
-c = c.replace(/  const \[activeGameIdx, setActiveGameIdx\] = useState\(0\);\n/, '');
-c = c.replace(/  const \[showMobileDetails, setShowMobileDetails\] = useState\(false\);\n/, '');
-c = c.replace(/  const activeGame = filteredGames\[activeGameIdx\];\n/, '');
-c = c.replace(/\/\/ [^\n]+\n  const gridRef = useRef[^\n]+;\n  const \{[^}]+\} = useGridNav\(\{[^}]+\}\);[\s\S]+?setFocusedIndex\(activeGameIdx\);\n  \}, \[activeGameIdx, focusedIndex, setFocusedIndex\]\);\n/, '');
-fs.writeFileSync('client/src/components/dashboard-themes/HomeArcadeTheme.tsx', c);
-console.log('Done');
