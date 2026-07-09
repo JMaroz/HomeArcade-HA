b565c30569977bcad54fc5ad4c50c4c9efff0699
Andrea Marozzi <andmaroz89@gmail.com>
2026-07-09 21:50:46 +0200

    fix: import getAbsoluteFilePath directly from ../utils instead of re-export chain

--- cabinet_bridge/server/routes/utils.ts
+++ cabinet_bridge/server/routes/utils.ts
@@ -1,7 +1,5 @@
 import path from "node:path";
 import zlib from "node:zlib";
-import { getAbsoluteFilePath } from "../utils";
-export { getAbsoluteFilePath };
 import { ROM_EXTENSIONS } from "./shared";
 import { FOLDER_TO_SYSTEM } from "../scanner";
 
